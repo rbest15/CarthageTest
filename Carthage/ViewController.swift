a459d58a01a226cf1df54957287ec38cfc1754ea
moremist <rbest15@rambler.ru>
2020-02-01 15:36:36 +0300

    test 1

--- Carthage/ViewController.swift
+++ Carthage/ViewController.swift
@@ -7,14 +7,15 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        AF.request("http://api.openweathermap.org/data/2.5/forecast?q=Krasnodar&appid=399e9ee290d3b3f3a2b179c3162f9b41").response { (response) in
+
+        }
     }
-
-
 }
 
